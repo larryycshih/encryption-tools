18be653b7ded2af7cdd13b8df3cab829b77953d3
Larry <Larry@Larry-Laptop>
2016-07-26 14:11:02 +1000

    fixed the CI equation.

--- src/CoIncident_Index.java
+++ src/CoIncident_Index.java
@@ -5,82 +5,82 @@ public class CoIncident_Index {
 	private double CI;
 	private int c;
 	private double[] period;
-	private int p_size = 15;
+	private int p_size = 20;
+
 	public CoIncident_Index(String msg) {
 		// TODO Auto-generated constructor stub
 		this.msg = msg;
 		this.c = 26;
-		processing();
+		CI = calcCI(msg);
 		period = new double[p_size];
 		findPeriod();
 	}
-	
-	private void processing(){
-		FreqDistribution f = new FreqDistribution(msg);
-		double N = f.getFreqDistrib().size();
-		double tmp = 0.0;
-		//iterate over all key pairs and add them all into tmp
 
-		for (Map.Entry<Character,Integer> entry : f.getFreqDistrib().entrySet()){
-			double v = entry.getValue();
-			
-			//tmp += ((v / N) * ((v-1)/(N-1)));
-			tmp += (v * (v-1)) / (N*(N-1))/c;
-		}
-		CI = tmp;
-	}
-	
-	private double calcCI(String m){
+	private double calcCI(String m) {
 		FreqDistribution f = new FreqDistribution(m);
-		double N = f.getFreqDistrib().size();
+		double N = m.length();
 		double tmp = 0.0;
-		//iterate over all key pairs and add them all into tmp
+		// iterate over all key pairs and add them all into tmp
 
-		for (Map.Entry<Character,Integer> entry : f.getFreqDistrib().entrySet()){
+		for (Map.Entry<Character, Integer> entry : f.getFreqDistrib().entrySet()) {
 			double v = entry.getValue();
+			//these equation all seem to be wrong seriously wtf
 			
-			//tmp += ((v / N) * ((v-1)/(N-1)));
-			tmp += (v * (v-1)) / (N*(N-1))/c;
+			// tmp += ((v / N) * ((v-1)/(N-1)));
+			// tmp += (v * (v - 1)) / (N * (N - 1)) / c;
+			tmp = tmp+ v*(v-1);
 		}
+		tmp = tmp / (N*(N-1));
 		return tmp;
 	}
-	
-	private void findPeriod(){
-		//sort msg into arrays with with each character at intervals of x
-		int i = 0;
-		for (String s : sortMsgByLength(3)){
-			period[i] = calcCI(s);
-			i++;
+
+	private void findPeriod() {
+		// sort msg into arrays with with each character at intervals of x
+		// for 1 to 15 calc CI averages for each intervals
+
+		for (int i = 0; i < period.length; i++) {
+			double average = 0;
+			String[] str = sortMsgByLength(i + 1);
+			for (String s : str) {
+				average += calcCI(s);
+			}
+			average = average / str.length;
+			period[i] = average;
 		}
-		
+
 	}
-	
-	private String[] sortMsgByLength(int x){
-		String [] str = new String[x];
-		str[0] = "";
-		str[1] = "";
-		str[2] = "";
+
+	private String[] sortMsgByLength(int x) {
+		String[] str = new String[x];
+		// it seems i need to init the array
+		for (int i = 0; i < str.length; i++) {
+			str[i] = "";
+		}
+		// split the message into multiple arrays
 		int j = 0;
-		for (int i = 0; i < msg.length(); i++){
-			if (j >= x) j =0;
+		for (int i = 0; i < msg.length(); i++) {
+			if (j >= x)
+				j = 0;
 			str[j] += msg.charAt(i);
 			j++;
 		}
 		return str;
 	}
-	
-	
-	public double getCI(){return CI;}
-	
-	public double[] getPeriod(){return period;}
-	
-	public String toString(){
-		String str = "CI: "+CI + "\nPeriods\n";
-		for (int i = 0 ; i < 15 ;i++){
-			str += i+1+":"+period[i]+"\n"; 
+
+	public double getCI() {
+		return CI;
+	}
+
+	public double[] getPeriod() {
+		return period;
+	}
+
+	public String toString() {
+		String str = "CI, " + CI + "\nPeriods\n";
+		for (int i = 0; i < period.length; i++) {
+			str += i + 1 + "," + period[i] + "\n";
 		}
 		return str;
 	}
-	
 
 }
